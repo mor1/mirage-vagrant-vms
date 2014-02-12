0be6d0323a17588ce848548ba89f907b485fda4e
Richard Mortier <mort@cantab.net>
2014-02-12 00:03:55 +0000

    first cut at a wheezy/xen definition

---
+++ definitions/debian-7.4.0-xen/xen.sh
@@ -0,0 +1,16 @@
+apt-get -y update
+
+apt-get install xen-linux-system
+
+dpkg-divert --divert /etc/grub.d/08_linux_xen --rename /etc/grub.d/20_linux_xen
+
+update-grub
+
+echo >> /etc/network/interfaces <<EOF
+auto xenbr0
+iface xenbr0 inet dhcp
+   bridge_ports eth0
+EOF
+
+## after reboot
+# apt-get install xen-tools
