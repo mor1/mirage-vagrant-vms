b9665908be1ba497573301175fbf99aea4da67f9
Richard Mortier <mort@cantab.net>
2014-02-15 00:23:34 +0000

    [provisioning] remove `opam-src` after install; add my rc files

---
+++ provisioning/mort.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+git clone git://github.com/mor1/rc-files src/rc-files || true
+chown -R vagrant:vagrant src
+
+cd src/rc-files
+rm -f ~vagrant/.bashrc
+mv ~vagrant/.ssh/authorized_keys ./ssh && rm -rf ~vagrant/.ssh
+sudo -u vagrant ./install.sh
--- provisioning/opam.sh
+++ provisioning/opam.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-su - vagrant
-
 OPAM_VER=1.1.1
 OPAM_PKG=opam-full-${OPAM_VER}.tar.gz
 OPAM_URL=https://github.com/ocaml/opam/releases/download/${OPAM_VER}/${OPAM_PKG}
@@ -20,9 +18,16 @@ tar xzvf opam-full-1.1.1.tar.gz --strip-components=1
 ./configure
 make
 make install
+cd ..
+rm -rf opam-src
 
 opam init --yes --auto-setup
 eval $(opam config env)
 
 opam switch --yes 4.01.0
+eval $(opam config env)
+
 opam install --yes mirage utop
+
+mv ~root/.opam ~vagrant/
+chown -R vagrant:vagrant ~vagrant/.opam
