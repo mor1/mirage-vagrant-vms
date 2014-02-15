d2ead276985484b747e70abb6c973462581f4152
Richard Mortier <mort@cantab.net>
2014-02-15 00:36:50 +0000

    [provisioning] backports for git 1.8, plus tidy up opam install

--- provisioning/opam.sh
+++ provisioning/opam.sh
@@ -6,28 +6,21 @@ OPAM_VER=1.1.1
 OPAM_PKG=opam-full-${OPAM_VER}.tar.gz
 OPAM_URL=https://github.com/ocaml/opam/releases/download/${OPAM_VER}/${OPAM_PKG}
 
-if [ ! -r "opam-src/$OPAM_PKG" ] ; then
-    mkdir -p opam-src
-    wget $OPAM_URL
-    mv $OPAM_PKG opam-src
-fi
-
+mkdir -p opam-src
 cd opam-src
+
+wget $OPAM_URL
 tar xzvf opam-full-1.1.1.tar.gz --strip-components=1
 
 ./configure
 make
 make install
+
 cd ..
 rm -rf opam-src
 
-opam init --yes --auto-setup
-eval $(opam config env)
-
-opam switch --yes 4.01.0
-eval $(opam config env)
-
-opam install --yes mirage utop
-
-mv ~root/.opam ~vagrant/
-chown -R vagrant:vagrant ~vagrant/.opam
+sudo -u vagrant -c "opam init --yes --auto-setup    \
+  && eval $(opam config env)                        \
+  && opam switch --yes 4.01.0                       \
+  && eval $(opam config env)                        \
+  && opam install --yes mirage utop"
--- provisioning/packages.sh
+++ provisioning/packages.sh
@@ -2,22 +2,13 @@
 
 set -ex
 
-apt-get install -y build-essential m4 git                    # build tools
+## backports good -- git 1.8 please
+BACKPORTS=http://ftp.uk.debian.org/debian
+echo "deb $BACKPORTS wheezy-backports main" >> /etc/apt/sources.list
+apt-get update
+apt-get -t wheezy-backports install "git"
+
+apt-get install -y build-essential m4                        # build tools
 apt-get install -y xen-tools                                 # xen client tools
 apt-get install -y ocaml ocaml-native-compilers camlp4-extra # ocaml
 apt-get install -y aspcud                                    # opam solver
-
-## virtualbox guest additions
-
-# apt-get install -y module-assistant || true
-# m-a prepare
-# autopoint debhelper gettext html2text intltool-debian kbuild libcroco3 libgettextpo0 libmail-sendmail-perl libsys-hostname-long-perl libunistring0 po-debconf virtualbox-source
-# sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
-# $ wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-# $ sudo apt-get update
-# $ sudo apt-get install virtualbox-4.3
-
-# apt-get install -y virtualbox-guest-dkms
-
-# mount /media/cdrom
-# sh /media/cdrom/VBoxLinuxAdditions.run
