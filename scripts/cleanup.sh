560a84c712364c536a749b6ac99d1fdbb2e6c771
Ian Duffy <ian@ianduffy.ie>
2014-06-15 18:02:26 +0100

    Use @djs55 's cleanup script for reseting the interfaces

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,14 +1,15 @@
 rm -rf /tmp/hotfixes
 
-cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
-DEVICE=eth0
-BOOTPROTO=dhcp
-ONBOOT=yes
-EOF
-
 xe host-management-disable
-managementuuid=`xe pif-list --minimal`
-xe pif-forget uuid=$managementuuid
+IFS=","; for pif in $(xe pif-list params=uuid --minimal); do
+  xe pif-unplug uuid=$pif
+  xe pif-forget uuid=$pif
+done
 
-ifconfig eth0 up
-dhclient eth0
+# add eth0
+. /etc/xensource-inventory
+xe pif-scan host-uuid=${INSTALLATION_UUID}
+PIF=$(xe pif-list device=eth0 params=uuid --minimal)
+xe pif-reconfigure-ip uuid=${PIF} mode=dhcp
+xe pif-plug uuid=${PIF}
+# other interfaces configured after boot
