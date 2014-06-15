fa0824eda9f5eba7fe2c1169102cb4f6027489cf
Ian Duffy <ian@ianduffy.ie>
2014-06-15 18:42:24 +0100

    Re-arrange scripts to ensure a under 1gb box file

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,10 +6,11 @@ IFS=","; for pif in $(xe pif-list params=uuid --minimal); do
   xe pif-forget uuid=$pif
 done
 
-# add eth0
 . /etc/xensource-inventory
 xe pif-scan host-uuid=${INSTALLATION_UUID}
 PIF=$(xe pif-list device=eth0 params=uuid --minimal)
 xe pif-reconfigure-ip uuid=${PIF} mode=dhcp
 xe pif-plug uuid=${PIF}
-# other interfaces configured after boot
+
+ifconfig eth0 up
+dhclient eth0
--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,2 +1,4 @@
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+
+sync; sync; sync
