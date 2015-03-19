62265d34e6fbe309546e7982ace2535195c9a645
Richard Mortier <mort@cantab.net>
2015-03-19 23:50:57 +0000

    xen: copyright; correct package names (ubuntu)

--- scripts/xen.sh
+++ scripts/xen.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
-#
-# Copyright (c) 2015 Richard Mortier <mort@cantab.net>
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without fee is hereby granted, provided that the above copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
 
-apt-get install -y xen-linux-system xen-system-amd64 xen-tools
+apt-get install -y xen-hypervisor-4.4-amd64 bridge-utils xen-tools
 
 dpkg-divert --divert /etc/grub.d/08_linux_xen --rename /etc/grub.d/20_linux_xen
 
