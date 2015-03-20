5814a848b777aa969a4b1fb205804959aa2678ab
Richard Mortier <mort@cantab.net>
2015-03-20 13:25:47 +0000

    opam: create `0install` dir due to old bug

--- scripts/opam.sh
+++ scripts/opam.sh
@@ -4,6 +4,8 @@ set -ex
 
 rm -rf ~/.config ~/bin
 
+mkdir ~/bin || true
+
 sudo apt-get install -y ocaml ocaml-native-compilers camlp4-extra # ocaml
 sudo apt-get install -y aspcud curl zeroinstall-injector          # 0install
 0install add opam http://tools.ocaml.org/opam.xml                 # opam
